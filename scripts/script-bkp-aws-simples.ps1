7972ccb2f182008fde9a36dee37c7197ad6c22cb
Sâmeque Mendes <64985406+samequemendes@users.noreply.github.com>
2022-04-28 17:09:33 -0300

    comandos adcionais
    comandos para sincronizar e copiar com recursive

--- scripts/script-bkp-aws-simples.ps1
+++ scripts/script-bkp-aws-simples.ps1
@@ -1,1 +1,8 @@
-﻿aws s3 sync c:/aws s3://treinamento.bkp > /result.txt
+# PARA SINCRONIZAR
+# SINTAXE: aws s3 sync (bucket.aws} {pasta.local}
+# ex. restaurar backup bucket para pc em pasta específica
+aws s3 sync s3://treinamento.bkp c:/AWS
+
+# SINTAXE: aws s3 sync (bucket.aws} {pasta.local}
+# ex. copiar arquivos em pasta para bucket - utilize a flag --recursive e assim envia todos os arquivos
+aws s3 cp c:/AWS s3://treinamento.bkp --recursive
